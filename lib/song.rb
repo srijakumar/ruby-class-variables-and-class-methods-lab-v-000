96a1b3baef17837ce1404de1b3cbb72d87f745ee
Srija S <srijakumar7@gmail.com>
2018-04-24 03:00:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,21 +27,17 @@ def self.artist
 
 end
 
-
 def self.genre_count
-  genre_count = {}
-  @@genre.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
-    else 
-      genre_count[genre] = 1
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
-end
-genre_count
-end
-
-
-
 
 
 end
