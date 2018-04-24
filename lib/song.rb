16ffcd019d32e6d55b345bf6c98adb1f3328a227
Srija S <srijakumar7@gmail.com>
2018-04-24 03:03:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,44 @@
 class Song
-attr_accessor :name, :artist, :genre
-@@count = 0
-@@genre = []
-@@artists = []
+  attr_accessor :name, :artist, :genre
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count += 1
-  @@genre << genre
-  @@artists << artist
-end
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
-def self.count
-  @@count
-end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
 
-def self.genre
-@@genre.uniq
-end
+  def self.count
+    @@count
+  end
 
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.artist
-  @@artist.uniq
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
-end
+  def self.genres
+    @@genres.uniq
+  end
 
-def self.genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
@@ -38,19 +49,4 @@ def self.genre_count
     end
     genre_count
   end
-
-def self.artist_count
-  artist_count = {}
-  @@artist.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1
-    else artist_count[artist] = 1
-    end
-  end
-  artist_count
-end
-
-
-
-
 end
